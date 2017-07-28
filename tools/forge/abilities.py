9eaf45f998f01fa358e7a720fee44fea174c0a43
the-nick-of-time <nith2593@colorado.edu>
2017-07-28 16:12:21 -0600

    Polished the abilities handler

--- tools/forge/abilities.py
+++ tools/forge/abilities.py
@@ -7,18 +7,17 @@ import rolling as r
 import tkUtility as util
 
 class AbilityDisplay(gui.Section):
+    abilnames = ['strength', 'dexterity', 'constitution',
+                 'intelligence', 'wisdom', 'charisma']
     def __init__(self, master, character):
         gui.Section.__init__(self, master)
         self.person = character
         self.subf = tk.Frame(self.f)
         #############
-        self.abilnames = ['strength', 'dexterity', 'constitution',
-                          'intelligence', 'wisdom', 'charisma']
         self.abilchecks = [tk.Button(self.subf, text=a[:3].upper(), command=lambda x=a: self.roll_check(x), width=4) for (i, a) in enumerate(self.abilnames)]
-        # self.scores = [util.labeledEntry(self.f, a[:3].upper(), i, 0, orient='h', width=4) for (i, a) in enumerate(self.abilnames)]
-        self.scores = [tk.Entry(self.subf, width=4) for a in self.abilnames]
+        self.abiltexts = [tk.StringVar() for a in self.abilnames]
+        self.scores = [tk.Entry(self.subf, width=4, textvariable=self.abiltexts[i]) for (i, a) in enumerate(self.abilnames)]
         self.mods = [tk.Label(self.subf, width=2) for a in self.abilnames]
-        # self.abilcheck = [tk.Button(self.f, text='Check', command=) for (i, a) in enumerate(self.abilnames)]
         self.adv = tk.BooleanVar()
         self.advbutton = tk.Checkbutton(self.f, text='Advantage?', variable=self.adv)
         self.dis = tk.BooleanVar()
@@ -34,6 +33,7 @@ class AbilityDisplay(gui.Section):
             util.replaceEntry(self.scores[i], self.person.abilities[a])
             self.scores[i].grid(row=i, column=1)
             self.mods[i].grid(row=i, column=2)
+            self.abiltexts[i].trace('w', lambda a, b, c: self.update_character())
         self.advbutton.grid(row=1, column=0)
         self.disbutton.grid(row=2, column=0)
         self.rolldisplay.grid(row=3, column=0)
@@ -53,7 +53,7 @@ class AbilityDisplay(gui.Section):
 
     def update_character(self):
         for (i, a) in enumerate(self.abilnames):
-            self.person.abilities[a] = int(self.scores[i].get())
+            self.person.abilities[a] = int(self.scores[i].get() if self.scores[i].get() else 0)
 
 if __name__ == '__main__':
     win = tk.Tk()
@@ -62,4 +62,9 @@ if __name__ == '__main__':
     jf = JSONInterface('character/Calan.character')
     app = AbilityDisplay(win, Character(jf))
     app.pack()
+    def quit(app, win):
+        app.person.write()
+        win.destroy()
+    WQ = tk.Button(win, text='QUIT', command=lambda: quit(app, win))
+    WQ.pack()
     win.mainloop()
