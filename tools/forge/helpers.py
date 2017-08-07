5f607a766baf1dc34f0abc0e02d01583ec807744
the-nick-of-time <nith2593@colorado.edu>
2017-08-06 22:27:53 -0600

    Added roll constants and path_follower to helpers

--- tools/forge/helpers.py
+++ tools/forge/helpers.py
@@ -7,6 +7,13 @@ from classes import *
 with open('./conditions.json') as f:
     condition_defs = json.load(f)
 
+D20 = '1d20'
+D20_LUCK = '1d20r1'
+ADV = '2d20h1'
+ADV_LUCK = '2d20r1h1'
+DIS = '2d20l1'
+DIS_LUCK = '2d20r1l1'
+
 
 def modifier(score):
     return (int(score) - 10) // 2
@@ -72,7 +79,7 @@ def type_select(extension):
 
 
 def find_file(name, type_):
-    from .interface import JSONInterface
+    from interface import JSONInterface
     directory = '{direc}/{name}'
     location = type_.split(sep='.')
     if (location[0] == ''):
@@ -84,7 +91,23 @@ def find_file(name, type_):
     filename = directory.format(
         direc=location[-1], name='.'.join(location))
     try:
-        iface = JSONInterface(filename, PREFIX=name if deeper else '')
+        iface = JSONInterface(filename)
         return iface
+    except FileNotFoundError:
+        raise
+
+
+def path_follower(path):
+    from interface import JSONInterface
+    match = re.match('/*(\w*.*\.[a-z]*)(.*)', path)
+    try:
+        tofile = match.group(0)
+        infile = match.group(1)
+    except IndexError:
+        raise
+    # if (os.path.isfile(JSONInterface.OBJECTSPATH + tofile)):
+    try:
+        jf = JSONInterface(tofile)
+        return (jf, infile)
     except FileNotFoundError:
         raise
