5e6523db22583748f2f8edf7a7a6388e6594204f
the-nick-of-time <nith2593@colorado.edu>
2017-08-07 20:26:48 -0600

    All missing spell components copied in and formatted correctly

--- tools/forge/SpellAttacks.py
+++ tools/forge/SpellAttacks.py
@@ -6,11 +6,12 @@ import helpers as h
 
 iface.JSONInterface.OBJECTSPATH = '../objects/'
 
-roll_pattern = '(\d+d\d+)\s?\+?\s?\d*'
-# roll_pattern = '(\d+d\d+)'
-damage_roll_pattern = roll_pattern + ' (\w+) damage'
-saving_throw_pattern = '(Strength|Dexterity|Constitution|Intelligence|Wisdom|Charisma) saving throw'
-attack_flag_pattern = 'spell attack'
+# roll_pattern = '(\d+d\d+)\s?\+?\s?\d*'
+# # roll_pattern = '(\d+d\d+)'
+# damage_roll_pattern = roll_pattern + ' (\w+) damage'
+# saving_throw_pattern = '(Strength|Dexterity|Constitution|Intelligence|Wisdom|Charisma) saving throw'
+# attack_flag_pattern = 'spell attack'
+component_pattern = '(V)?(S)?(M)?(gp)?'
 
 # In the end, needs to set:
 # /damage to damage dice
@@ -21,20 +22,32 @@ attack_flag_pattern = 'spell attack'
 for f in os.scandir('../objects/spell/'):
     print(f.name)
     jf = iface.JSONInterface('spell/' + f.name)
-    eff = jf.get('/effect')
-    attackroll = re.search(attack_flag_pattern, eff) is not None
-    if (attackroll):
-        jf.set('/attack_roll', attackroll)
-    else:
-        save = re.search(saving_throw_pattern, eff)
-        if (save is not None):
-            jf.set('/save', save.group(1))
-    damageroll = re.search(damage_roll_pattern, eff)
-    if (damageroll is not None):
-        jf.set('/damage', damageroll.group(1))
-        jf.set('/damage_type', damageroll.group(2))
-    else:
-        roll = re.search(roll_pattern, eff)
-        if roll:
-            jf.set('/damage', roll.group(1))
-    jf.write()
+    # eff = jf.get('/effect')
+    # attackroll = re.search(attack_flag_pattern, eff) is not None
+    # if (attackroll):
+    #     jf.set('/attack_roll', attackroll)
+    # else:
+    #     save = re.search(saving_throw_pattern, eff)
+    #     if (save is not None):
+    #         jf.set('/save', save.group(1))
+    # damageroll = re.search(damage_roll_pattern, eff)
+    # if (damageroll is not None):
+    #     jf.set('/damage', damageroll.group(1))
+    #     jf.set('/damage_type', damageroll.group(2))
+    # else:
+    #     roll = re.search(roll_pattern, eff)
+    #     if roll:
+    #         jf.set('/damage', roll.group(1))
+    # jf.write()
+    comp = jf.get('/components')
+    result = re.fullmatch(component_pattern, comp)
+    if (result is not None):
+        new = []
+        for item in result.groups():
+            if (item):
+                new.append(item)
+        # result = ((item if item) for item in result.groups())
+        newcomp = ', '.join(new)
+        print(newcomp)
+        jf.set('/components', newcomp)
+        jf.write()
