ff682d846034d444958d6d62d5545cb39d52192c
the-nick-of-time <nith2593@colorado.edu>
2017-08-11 15:09:11 -0600

    Added many more monsters, thanks to ceryliae Files from the bestiary of https://github.com/ceryliae/DnDAppFiles

--- tools/scripts/monsterparser.py
+++ tools/scripts/monsterparser.py
@@ -1,4 +1,5 @@
 import json
+import re
 import os
 import sys
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + '/../libraries')
@@ -8,11 +9,44 @@ import helpers as h
 
 JSONInterface.OBJECTSPATH = '../objects/'
 
-with open('../objects/5e-SRD-Monsters.json', 'r') as infile:
-    core = json.load(infile)
+from xml.etree import cElementTree as ElementTree
 
 
-for monster in core[:-1]:
+monster = {}
+dry = False
+# t = ElementTree.parse("../objects/Volo's Bestiary.xml")
+# t = ElementTree.parse("../../../DnDAppFiles/Bestiary/Curse of Strahd Bestiary.xml")
+# t = ElementTree.parse("../../../DnDAppFiles/Bestiary/Hoard of the Dragon Queen Bestiary.xml")
+# t = ElementTree.parse("../../../DnDAppFiles/Bestiary/Out of the Abyss.xml")
+# t = ElementTree.parse("../../../DnDAppFiles/Bestiary/Phandelver Bestiary.xml")
+# t = ElementTree.parse("../../../DnDAppFiles/Bestiary/Princes of the Apocalypse Bestiary.xml")
+# t = ElementTree.parse("../../../DnDAppFiles/Bestiary/Storm King's Thunder.xml")
+# t = ElementTree.parse("../../../DnDAppFiles/Bestiary/Tales From the Yawining Portal Bestiary.xml")
+t = ElementTree.parse("../../../DnDAppFiles/Bestiary/The Rise of Tiamat Bestiary.xml")
+
+
+root = t.getroot()
+for item in root:
+    # print(item[0].text)
+    monster['name'] = item[0].text
+    # print(item[4].text)
+    monster['armor_class'] = item[4].text
+    # print(item[5].text)
+    s = item[5].text
+    m = re.search('\(.+\)', s).group(0).strip('()')
+    monster['hit_dice'] = m
+    for name, abil in zip(['strength', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'charisma'], item[7:13]):
+        # print(abil.text)
+        monster[name] = int(abil.text)
+    # print(monster)
+    # for child in item:
+    #     print(child.text)
+
+# with open('../objects/5e-SRD-Monsters.json', 'r') as infile:
+#     core = json.load(infile)
+#
+#
+# for monster in core[:-1]:
     print(monster['name'])
     data = {
         "name": monster['name'],
@@ -27,11 +61,14 @@ for monster in core[:-1]:
             "Charisma": monster['charisma']
         }
     }
+    # print(data)
     # TODO: add in skills after implementing that in monsters.py
-    # s = json.dumps(data, indent=2)
-    # print(s)
-    formatstr = '{}monster/{}.monster'
-    filename = formatstr.format(JSONInterface.OBJECTSPATH,
-                                h.clean(monster['name']))
-    with open(filename, 'w') as outfile:
-        json.dump(data, outfile, indent=2)
+    if dry:
+        s = json.dumps(data, indent=2)
+        print(s)
+    else:
+        formatstr = '{}monster/{}.monster'
+        filename = formatstr.format(JSONInterface.OBJECTSPATH,
+                                    h.clean(monster['name']))
+        with open(filename, 'w') as outfile:
+            json.dump(data, outfile, indent=2)
