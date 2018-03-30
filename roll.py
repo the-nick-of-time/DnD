3e0e6cd6f8b5f8aa90b9424a96f1e732e850417a
Nicholas Thurmes <nith2593@colorado.edu>
2018-03-30 00:16:49 -0600

    Switch to using optparse on command line roll Roll really should be updated to allow combination of arguments like "verbose" and "average"

--- roll.py
+++ roll.py
@@ -2,6 +2,7 @@
 
 import sys
 import re
+from optparse import OptionParser
 
 import tools.libraries.rolling as r
 
@@ -52,7 +53,38 @@ def main():
             i += 1
 
 if (__name__ == '__main__'):
-    try:
-        main()
-    except:
-        usage()
+    parser = OptionParser(usage="usage: %prog [options] expr [expr...]")
+    parser.add_option("-a", "--average",
+                      action="store_true", dest="average",
+                      help="Take the average roll")
+    parser.add_option("-c", "--critical",
+                      action="store_true", dest="crit",
+                      help="Roll as a critical hit (number of dice doubled)")
+    parser.add_option("-v", "--verbose",
+                      action="store_true", dest="verbose",
+                      help="Roll in verbose mode (shows rolls, constant modifiers, and total)")
+    parser.add_option("-n", "--number",
+                      dest="number", type="int",
+                      help="Roll each roll this number of times")
+    parser.set_defaults(number=1, verbose=False, crit=False)
+    (options, args) = parser.parse_args()
+    fmt = "{0:2}"
+    width = 16
+    if len(args) == 0:
+        print(fmt.format(r.roll("1d20")))
+    else:
+        if options.verbose:
+            if options.crit:
+                op = "multipass_critical"
+            else:
+                op = "multipass"
+        elif options.average:
+            op = "average"
+        else:
+            op = "execute"
+        for expr in args:
+            for i in range(options.number):
+                if (i%width == 0 and i != 0):
+                    print()
+                print(fmt.format(r.roll(expr, option=op)), end="  ")
+            print()
