184feb6de5261e39ec5f7a840b5f95d921a23fb4
Nick Thurmes <nthurmes@gmail.com>
2019-09-11 20:51:01 -0600

    HP and HD handlers Along with getting resources into a lib

--- tools/libraries/hpLib.py
+++ tools/libraries/hpLib.py
@@ -1,2 +1,120 @@
-class HPHandler:
-    pass
+from math import ceil
+
+from dndice import basic
+
+from classes import Character
+from exceptionsLib import LowOnResource
+from helpers import modifier
+from interface import JSONInterface
+from resourceLib import Resource
+from settingsLib import Settings, RestLengths, HealingMode
+
+
+class HP:
+    def __init__(self, character: Character):
+        self.owner = character
+        self.record = character.record
+        self.hd = {size: HD(self.record, size, character) for size in self.record.get('/HP/HD')}
+
+    @property
+    def current(self):
+        return self.record.get('/HP/current')
+
+    @current.setter
+    def current(self, value):
+        if isinstance(value, int):
+            self.record.set('/HP/current', value)
+        else:
+            raise TypeError('Trying to set HP to not a number')
+
+    @property
+    def max(self):
+        return self.record.get('/HP/max')
+
+    @max.setter
+    def max(self, value):
+        self.record.set('/HP/max', value)
+
+    @property
+    def temp(self):
+        return self.record.get('/HP/temp')
+
+    @temp.setter
+    def temp(self, value):
+        self.record.set('/HP/temp', value)
+
+    def change(self, amount):
+        """Change the HP
+
+        :param amount: A rollable amount
+        :return: The actual change in HP
+        """
+        delta = basic(amount)
+        if delta == 0:
+            return 0
+        if delta < 0:
+            if abs(delta) > self.temp:
+                # overflow beyond temp
+                delta += self.temp
+                self.temp = 0
+                self.current += delta
+                return delta
+            else:
+                # Temp absorbs it all
+                self.temp += delta
+                return 0
+        else:
+            # healing
+            if self.current + delta > self.max:
+                delta = self.max - self.current
+            self.current += delta
+            return delta
+
+    def add_temp(self, amount):
+        """Add some temp HP
+
+        :param amount: A rollable amount
+        :return: The actual change in HP (always 0 because temp HP is not real)
+        """
+        delta = basic(amount)
+        if delta > self.temp:
+            self.temp = delta
+        return 0
+
+    def rest(self, length):
+        if length == RestLengths.LONG:
+            if Settings.healing in (HealingMode.VANILLA, HealingMode.FAST):
+                self.current = self.max
+            self.temp = 0
+        for size in self.hd.values():
+            size.rest(length)
+
+
+class HD(Resource):
+    def __init__(self, jf: JSONInterface, size: str, character: Character):
+        super().__init__(jf, '/HP/HD/' + size, character=character)
+        self.name = 'Hit Die'
+        self.value = size
+        self.recharge = RestLengths.LONG
+
+    @property
+    def maxnumber(self):
+        return self.owner.classes.maxHD[self.value]
+
+    def use(self, _):
+        try:
+            roll = super().use(1)
+        except LowOnResource:
+            return 0
+        conmod = modifier(self.record.get('/abilities/Constitution'))
+        return roll + conmod if (roll + conmod > 1) else 1
+
+    def rest(self, length):
+        if length == RestLengths.LONG:
+            if Settings.healing == HealingMode.FAST:
+                self.reset()
+            else:
+                self.regain(ceil(self.maxnumber / 2))
+        if length == RestLengths.SHORT:
+            if Settings.healing == HealingMode.FAST:
+                self.regain(ceil(self.maxnumber / 4))
---
+++ tools/libraries/resourceLib.py
@@ -0,0 +1,91 @@
+import re
+
+from dndice import basic
+
+from classes import Character
+from exceptionsLib import LowOnResource
+from interface import JSONInterface
+from settingsLib import RestLengths
+
+
+class Resource:
+    def __init__(self, jf: JSONInterface, path: str, defjf=None, defpath=None, character=None):
+        self.record = jf
+        self.path = path
+        self.value = self._get('value')
+        self.recharge = RestLengths.from_string(self._get('recharge'))
+        self.definition = defjf if defjf is not None else jf
+        self.defpath = defpath if defpath is not None else path
+        self.name = self._def_get('name')
+        self._set_owner(character)
+
+    def _get(self, path):
+        return self.record.get(self.path + '/' + path)
+
+    def _set(self, path, value):
+        self.record.set(self.path + '/' + path, value)
+
+    def _def_get(self, path):
+        return self.definition.get(self.defpath + '/' + path)
+
+    def _set_owner(self, character: Character):
+        self.owner = character
+        if character is not None:
+            val = character.parse_vars(self.value, mathIt=False)
+            if (isinstance(val, str)):
+                pattern = r'\(.*\)'
+                rep = lambda m: str(basic(m.group(0)))
+                new = re.sub(pattern, rep, val)
+                self.value = new
+            else:
+                self.value = val
+
+    @property
+    def number(self):
+        return self._get('number')
+
+    @number.setter
+    def number(self, value):
+        self._set('number', value)
+
+    @property
+    def maxnumber(self):
+        val = self._get('maxnumber')
+        if (val is not None):
+            if (self.owner is not None):
+                return self.owner.parse_vars(val)
+            return val
+        if (self.owner is not None):
+            mx = self._def_get('maxnumber')
+            return self.owner.parse_vars(mx)
+        return self._def_get('maxnumber')
+
+    @maxnumber.setter
+    def maxnumber(self, value):
+        self.record.set(self.path + '/maxnumber', value)
+
+    def use(self, number):
+        if (self.number < number):
+            raise LowOnResource(self)
+        self.number -= number
+        if (isinstance(self.value, str)):
+            return basic('+'.join([self.value] * number))
+        elif (isinstance(self.value, int)):
+            return number * self.value
+        else:
+            return 0
+
+    def regain(self, number):
+        if (self.number + number > self.maxnumber):
+            self.reset()
+        else:
+            self.number += number
+
+    def reset(self):
+        self.number = self.maxnumber
+
+    def rest(self, length):
+        if length >= self.recharge:
+            self.reset()
+            return self.number
+        return -1
